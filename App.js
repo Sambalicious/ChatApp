9d9c3d505fbfdafccdb72ade35c62c994504e107
samuel Temitope <stemitope370@gmail.com>
2020-01-04 21:39:06 +0100

    add more javascript functionality using localStorage

--- App.js
+++ App.js
@@ -7,3 +7,36 @@ function openForm(){
 function closeForm(){
     document.getElementById('myForm').style.display='none'
 }
+
+
+let  messages = document.getElementById('messages');
+let textarea = document.getElementById('textarea');
+let button = document.getElementById('btn-send');
+
+
+button.addEventListener('click', function(e){
+    e.preventDefault();
+    let newMessage = document.createElement('li');
+    newMessage.innerHTML = textarea.value;
+    
+    messages.appendChild(newMessage);
+    
+
+    const text = document.getElementById('textarea').value;
+
+    let texts;
+
+    if (localStorage.getItem('texts') === null){
+        texts = [];
+
+    }
+
+    else {
+        texts = JSON.parse(localStorage.getItem('texts'));
+    }
+
+    texts.push(text);
+    localStorage.setItem('texts', JSON.stringify(texts));e
+
+    textarea.value = "";
+})
